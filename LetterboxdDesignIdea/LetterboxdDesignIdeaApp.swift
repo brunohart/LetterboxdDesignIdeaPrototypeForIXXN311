5472581c200d16945781b64816c85e0a0fb1f5fc
brunohart <123848709+brunohart@users.noreply.github.com>
2025-05-06 07:39:40 +1200

    Initial Commit

---
+++ LetterboxdDesignIdea/LetterboxdDesignIdeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterboxdDesignIdeaApp.swift
+//  LetterboxdDesignIdea
+//
+//  Created by Brunohart on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LetterboxdDesignIdeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LetterboxdDesignIdeaTests/LetterboxdDesignIdeaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetterboxdDesignIdeaTests.swift
+//  LetterboxdDesignIdeaTests
+//
+//  Created by Brunohart on 06/05/2025.
+//
+
+import Testing
+@testable import LetterboxdDesignIdea
+
+struct LetterboxdDesignIdeaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
